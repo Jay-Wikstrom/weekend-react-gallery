90cf66affaa39acef02e0d531f982f50e4d5df27
Jay Wikstrom <jaywikstrom@yahoo.com>
2021-08-12 21:21:31 -0500

    Add comments to App.js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,20 @@
 import axios from 'axios';
-import React, { useState, useEffect } from 'react';
 import GalleryList from '../GalleryList/GalleryList';
 import './App.css';
 
+//Object destructuring
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
-
+  //Array Destructuring
   const [photoList, setPhotoList] = useState([])
 
+  //Lifestyle hook gets called when component loads
   useEffect(() => {
     fetchPhotos();
   }, []);
+
 
   const fetchPhotos = () => {
     console.log('In Get');
@@ -29,9 +33,14 @@ function App() {
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
-        <GalleryList photoList={photoList} />
+
+        {/* Render GalleryList component */}
+        <GalleryList 
+          //Pass photoList prop to photoList component
+          photoList={photoList} 
+        />
       </div>
     );
-}
+} //end app function
 
 export default App;
