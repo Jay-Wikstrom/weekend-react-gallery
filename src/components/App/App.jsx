8b386906a7b7732f4925c8666c0277a37fba7196
Jay Wikstrom <jaywikstrom@yahoo.com>
2021-08-12 19:49:03 -0500

    GET route created on App.js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+
+  useEffect(() => {
+    fetchGalleryList();
+  }, []);
+
+  const [galleryList, setGalleryList] = useState([])
+
+  const fetchGalleryList = () => {
+    console.log('In Get');
+    axios({
+      method: 'GET',
+      url: '/gallery'
+    }).then(response => {
+      setGalleryList(response.data);
+    }).catch(error => {
+      console.log('GET /Gallery error', error)
+    })
+  }
+
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/MyDog.PNG"/>
+        {/* <img src="images/MyDog.PNG"/>
         <img src="images/OtherDog.PNG" />
         <img src="images/MyCat.PNG" />
         <img src="images/RoseBowl.jpg" />
         <img src="images/MyFavoriteFootballTeam.jpg" />
-        <img src="images/MyFavoriteBasketballTeam.jpg" />
+        <img src="images/MyFavoriteBasketballTeam.jpg" />  */}
       </div>
     );
 }
---
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -0,0 +1,11 @@
+
+function GalleryItem(){
+
+    return (
+        <>
+            <h1>Test Gallery Item</h1>
+        </>
+    )
+}
+
+export default GalleryItem;
